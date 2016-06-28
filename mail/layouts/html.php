a8df685f428984489d8da57d6d5d9652ce45733b
UribeNelson <javier3d52@hotmail.com>
2016-06-28 15:35:37 -0400

    Funcionalidad de contacto funcionando
    La funcionalidad de contacto por un formulario, está funcionando
    correctamente.

--- mail/layouts/html.php
+++ mail/layouts/html.php
@@ -14,6 +14,7 @@ use yii\helpers\Html;
     <?php $this->head() ?>
 </head>
 <body>
+	<!-- Esta linea es de comentario -->
     <?php $this->beginBody() ?>
     <?= $content ?>
     <?php $this->endBody() ?>
